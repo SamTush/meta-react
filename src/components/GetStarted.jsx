f467baa1772b246d45a92e791c9244455fec4f9f
Samson Tush <samo4421@icloud.com>
2023-07-28 10:17:47 +0300

    Add components

---
+++ src/components/GetStarted.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function GetStarted() {
+  return (
+    <div>
+      <button className='px-3 py-1 bg-red-600 text-white font-bold rounded-md '>
+        TEST
+      </button>
+    </div>
+  )
+}
+
+export default GetStarted
