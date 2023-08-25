0eb3dde5f0ce4e0d1b53e12860ee27cf7092c6ff
Samson Tush <samo4421@icloud.com>
2023-08-25 22:20:58 +0300

    Add github cards project

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <nav>
+        <ul>
+            <li>Add</li>
+            <li>GitHub</li>
+            <li>*</li>
+            <li>*</li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar
